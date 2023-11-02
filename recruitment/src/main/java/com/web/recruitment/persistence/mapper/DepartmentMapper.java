e32ab3e4620cf15dcd17752a9e9faf7017362fd9
KhanhToan14 <dkhanhtoan14@gmail.com>
2023-11-02 17:32:18 +0700

    Add department list

--- recruitment/src/main/java/com/web/recruitment/persistence/mapper/DepartmentMapper.java
+++ recruitment/src/main/java/com/web/recruitment/persistence/mapper/DepartmentMapper.java
@@ -3,6 +3,13 @@ package com.web.recruitment.persistence.mapper;
 import com.web.recruitment.persistence.dto.Department;
 import java.util.*;
 public interface DepartmentMapper {
-   /* List<Department> list(Map<String, Object> reqMap) throws Exception;*/
+    List<Department> list(Map<String, Object> reqMap) throws Exception;
+
     Department select(int id) throws Exception;
+
+    int total(Map<String, Object> reqMap) throws Exception;
+
+    int totalByKeyword(Map<String, Object> reqMap) throws Exception;
+
+    List<Department> listByName(Map<String, Object> reqMap) throws Exception;
 }
---
+++ recruitment/src/main/java/com/web/recruitment/utils/ConstantMessages.java
@@ -0,0 +1,17 @@
+package com.web.recruitment.utils;
+
+public class ConstantMessages {
+    // list method message
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String CURRENT_PAGE = "currentPage";
+    public static final String LIMIT = "limit";
+    public static final String OFFSET = "offset";
+    public static final String KEYWORD = "keyword";
+    public static final String TOTAL = "total";
+    public static final String DATA = "data";
+    public static final String SORT_TYPE = "sortType";
+    public static final String SORT_BY = "sortBy";
+
+    //
+    public static final String DEPARTMENT = "department";
+}
