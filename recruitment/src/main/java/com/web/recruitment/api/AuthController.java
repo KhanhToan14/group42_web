35bba6ec44d222c63369f1bcb6e97ee051aea6eb
toanphysics <nvttoan2017@gmail.com>
2023-11-30 09:37:10 +0700

    login logout

--- recruitment/src/main/java/com/web/recruitment/api/AuthController.java
+++ recruitment/src/main/java/com/web/recruitment/api/AuthController.java
@@ -1,84 +1,58 @@
 package com.web.recruitment.api;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import io.micrometer.common.util.StringUtils;
-import org.springframework.http.HttpHeaders;
+import com.web.recruitment.payload.request.LoginRequest;
+import com.web.recruitment.payload.response.MessageResponse;
+import com.web.recruitment.payload.response.UserInfoResponse;
+import com.web.recruitment.securiry.jwt.JwtUtils;
+import com.web.recruitment.securiry.services.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.web.recruitment.payload.request.LoginRequest;
-import com.web.recruitment.payload.response.MessageResponse;
-import com.web.recruitment.payload.response.UserInfoResponse;
-// import com.web.recruitment.persistence.dto.User;
-import com.web.recruitment.persistence.mapper.UserMapper;
-import com.web.recruitment.securiry.jwt.JwtUtils;
-import com.web.recruitment.securiry.services.UserDetailsImpl;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/v1/auth")
+@RequestMapping("/authen")
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
-
-    @Autowired
-    PasswordEncoder encoder;
-
     @Autowired
     JwtUtils jwtUtils;
-
-    @Autowired
-    UserMapper userMapper;
-
-    @PostMapping("/signin")
-    public ResponseEntity<Object> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        if (StringUtils.isEmpty(loginRequest.getPassword()) && StringUtils.isEmpty(loginRequest.getUsername())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username and password cannot be null or empty");
-        }
-
+    @PostMapping("/login")
+    public ResponseEntity<?> postExample(@Valid @RequestBody LoginRequest loginRequest) {
+        // Xử lý dữ liệu nhận được từ yêu cầu POST
+        // Ở đây, đơn giản là trả về dữ liệu đã nhận được
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getUsername(),
                         loginRequest.getPassword()
                 )
         );
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
         ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
-
         List<String> roles = userDetails.getAuthorities().stream()
                 .map(item -> item.getAuthority())
                 .collect(Collectors.toList());
-
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,
-                jwtCookie.toString())
+                        jwtCookie.toString())
                 .body(new UserInfoResponse(userDetails.getId(),
                         userDetails.getUsername(),
                         userDetails.getEmail(),
                         roles));
     }
-
-    @PostMapping("/signout")
+    @PostMapping("/logout")
     public ResponseEntity<?> logoutUser() {
         ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(new MessageResponse("You've been signed out!"));
+                .body(new MessageResponse("đã xóa cookie jwt!"));
     }
 }
